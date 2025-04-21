31f8f08beadce507b35cd3c67821ac52288f2284
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-04-21 19:53:20 +0200

    Add post_type for messages and validation rules for contact form

---
+++ wp-content/themes/portfolio/forms/ContactForm.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Portfolio\Forms;
+
+class ContactForm
+{
+
+    public static function handle(array $data): void
+    {
+        wp_insert_post([
+            'post_type' => 'contact_message',
+            'post_title' => $data['first_name'] . ' ' . $data['last_name'],
+            'post_content' => self::generateEmailContent($data),
+            'post_status' => 'publish'
+        ]);
+
+        wp_mail(
+            to: 'lorian.flamant05@gmail.com',
+            subject: 'Nouveau message de contact',
+            message: self::generateEmailContent($data),
+        );
+
+        $_SESSION['contact_form_success'] = 'Merci, ' . $data['firstname'] . '! Votre message a bien été envoyé.';
+        wp_safe_redirect($_SERVER['HTTP_REFERER']);
+        exit();
+    }
+
+    protected static function generateEmailContent(array $data): string
+    {
+        return 'Bonjour,' . PHP_EOL
+            . 'Vous avez un nouveau message de ' . $data['first_name'] . ' ' . $data['last_name'] . ':' . PHP_EOL
+            . $data['message'] . PHP_EOL . PHP_EOL
+            . '----' . PHP_EOL
+            . 'Adresse mail: ' . $data['email'];
+    }
+
+    public static function required(string $field_name): bool
+    {
+        if (
+            !array_key_exists($field_name, $_REQUEST)
+            || trim($_REQUEST[$field_name]) === ''
+        ) {
+            $_SESSION['errors'][$field_name] = 'Ce champ est obligatoire !';
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public static function email($field_name): bool
+    {
+        if (
+            array_key_exists($field_name, $_REQUEST) &&
+            trim($_REQUEST[$field_name]) !== '' &&
+            !filter_var(trim($_REQUEST[$field_name]), FILTER_VALIDATE_EMAIL)
+        ) {
+            $_SESSION['errors'][$field_name] = 'Veuillez renseigner une adresse mail valide !';
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public static function check(array $constraints): void
+    {
+        try {
+            self::parse_constraints($constraints);
+        } catch (ValidationRuleNotFoundException $e) {
+            exit($e->getMessage());
+        }
+
+        if (isset($_SESSION['errors'])) {
+            $_SESSION['old'] = $_REQUEST;
+            $previousUrl = $_SERVER['HTTP_REFERER'] ?? '/';
+            http_response_code('303');
+            header("Location: $previousUrl");
+            exit;
+        }
+    }
+
+    /**
+     * @throws ValidationRuleNotFoundException
+     */
+    protected static function parse_constraints(array $constraints): void
+    {
+        $method = '';
+        foreach ($constraints as $field_name => $constraint) {
+            $array_rules = explode('|', $constraint);
+            foreach ($array_rules as $method) {
+                if (!method_exists(__CLASS__, $method)) {
+                    throw new ValidationRuleNotFoundException($method);
+                }
+                self::$method($field_name);
+            }
+        }
+    }
+}
--- wp-content/themes/portfolio/functions.php
+++ wp-content/themes/portfolio/functions.php
@@ -1,6 +1,12 @@
 <?php
 
+// Start session
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 // Disable Gutenberg Editor
+
 add_filter('use_block_editor_for_post', '__return_false');
 add_filter('use_widgets_block_editor', '__return_false');
 add_action('wp_enqueue_scripts', function () {
@@ -10,8 +16,7 @@ add_action('wp_enqueue_scripts', function () {
 }, 20);
 
 
-
-// Add Custom Post Type for Projects
+// Add Custom Post Type for Projects and contact
 add_theme_support('post-thumbnails', ['project']);
 
 register_post_type('project', [
@@ -28,3 +33,78 @@ register_post_type('project', [
         'title', 'editor', 'excerpt', 'thumbnail',
     ],
 ]);
+
+register_post_type('contact_message', [
+    'label' => 'Messages',
+    'description' => 'Messages lié au formulaire de contact',
+    'menu_position' => 10, /*Position dans l'admin wordpress => Voir les position su internet*/
+    'menu_icon' => 'dashicons-email',
+    'public' => false,
+    'has_archive' => false, // Pour faire apparaitre les menus recettes et voyages dans les screen options de WordPress
+    'supports' => [
+        'title', 'editor',
+    ],
+    'show_ui' => true,
+]);
+
+
+// Navigation menu
+register_nav_menu('header', 'Le menu de navigation principale - FR');
+register_nav_menu('footer', 'Le menu de navigation secondaire - FR');
+
+function dw_get_navigation_links(string $location): array
+{
+    // 1°  Récupérer l'objet WordPress pour le menu à la location $location
+    $locations = get_nav_menu_locations();
+
+    if (!isset($locations[$location])) {
+        return [];
+    }
+
+    $nav_id = $locations[$location];
+    $nav = wp_get_nav_menu_items($nav_id);
+
+    // 2°  Transformer le menu en 1 tableau de lien , chaque lien étant un obet personnalisable
+    $links = [];
+    foreach ($nav as $post) {
+        $link = new stdClass(); // Créer un objet vide en PHP
+        $link->href = $post->url;
+        $link->label = $post->title;
+        $link->icon = get_field('icon', $post);
+
+        $links[] = $link; // Version raccourci pour pousser des élémnts dans un tableaux
+    }
+    return $links;
+    // 3°  Retourner ce tableau d'objet (liens)
+}
+
+// remove useless scripts and admin bar in UI
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('wp_head', 'wp_print_comments');
+remove_action('wp_head', 'wp_oembed_add_discovery_links');
+remove_action('wp_head', 'wp_oembed_add_host_js');
+remove_action('wp_head', 'rest_output_link_wp_head');
+remove_action('wp_head', 'wp_generator');
+
+add_filter('show_admin_bar', '__return_false');
+
+
+// Handling contact form
+use Portfolio\Forms\ContactForm;
+require __DIR__ . '/forms/ContactForm.php';
+
+add_action('admin_post_nopriv_dw_submit_contact_form', 'handle_contact_form'); // On est connecter
+add_action('admin_post_dw_submit_contact_form', 'handle_contact_form'); // On est pas connecter
+
+function handle_contact_form()
+{
+    ContactForm::check([
+        'last_name' => 'required',
+        'first_name' => 'required',
+        'email' => 'required|email',
+        'message' => 'required'
+    ]);
+
+    return ContactForm::handle($_REQUEST);
+}
