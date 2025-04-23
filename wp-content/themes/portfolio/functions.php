d0631b95bf39b0f3feee5b6b98ecdaa56205261e
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-04-23 20:46:30 +0200

    Add translation function

--- wp-content/themes/portfolio/functions.php
+++ wp-content/themes/portfolio/functions.php
@@ -146,5 +146,31 @@ function create_site_options_page()
         ]);
     }
 }
-
 add_action('acf/init', 'create_site_options_page');
+
+// Add text translation function
+
+function trans_load_textdomain(): void
+{
+    load_theme_textdomain('trans', get_template_directory() . '/locales');
+}
+
+add_action('after_setup_theme', 'trans_load_textdomain');
+
+function __trans(string $translation, array $replacements = [])
+{
+// 1. Récupérer la traduction de la phrase présente dans $translation
+    $base = __($translation, 'trans');
+
+// 2. Remplacer toutes les occurrences des variables par leur valeur
+    foreach ($replacements as $key => $value) {
+        $variable = ':' . $key;
+        $base = str_replace($variable, $value, $base);
+    }
+
+// 3. Retourner la traduction complète.
+    return $base;
+}
+
+
+// Ajouter la fonction SRCSET + Regarder pour l'adapter avec WEBP
