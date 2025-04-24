56b06a9638dc593f28bd0046d878af4947e18739
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-04-24 17:10:28 +0200

    Add final template to front-page

--- wp-content/themes/portfolio/front-page.php
+++ wp-content/themes/portfolio/front-page.php
@@ -1,19 +1,48 @@
 <?php get_header();
-if (have_posts()): while (have_posts()): the_post();
-    $stage = get_field('stage');
-    $content = get_field('content');
-    ?>
+if (have_posts()): while (have_posts()): the_post(); ?>
 
     <section>
-        <h2><?= $stage['main-title']; ?></h2>
+        <h2><?= __trans(get_field('main-title')); ?></h2>
         <div>
-            <h3><?= $stage['job-title']; ?></h3>
-            <p><?= $stage['job-description']; ?></p>
+            <h3><?= get_field('job-title'); ?></h3>
+            <p><?= get_field('job-description'); ?></p>
         </div>
         <figure>
             <img src="" alt="">
         </figure>
-        <a href="<?= $stage['high-link']['url']; ?>"><?= $stage['high-link']['title']; ?></a>
+        <a href="<?= get_field('high-link')['url']; ?>" title="Aller sur la page : À propos">
+            <?= get_field('high-link')['title']; ?>
+        </a>
+    </section>
+
+    <section>
+        <h2 class="sro">Mes derniers projets</h2>
+        <?php $projects = new WP_Query([
+            'post_type' => 'project',
+            'order' => 'DESC',
+            'orderby' => 'date',
+            'posts_per_page' => 2,
+        ]);
+
+        if ($projects->have_posts()): while ($projects->have_posts()): $projects->the_post(); ?>
+
+            <article>
+                <h3><?= get_the_title(); ?></h3>
+                <figure>
+                    <img src="" alt="">
+                </figure>
+                <a href="<?= get_the_permalink(); ?>" class="" title="Découvrir le projet">
+                    <span class="sro">Découvrir le projet :<?= get_the_title(); ?></span>
+                </a>
+            </article>
+
+        <?php endwhile; else: ?>
+            <p>Il n'y a aucun projets à voir pour le moment</p>
+        <?php endif; ?>
+        <?php wp_reset_postdata(); ?>
+        <a href="<?= get_field('low-link')['url']; ?>" title="Aller sur la page : Mes projets">
+            <?= get_field('low-link')['title']; ?>
+        </a>
     </section>
 
 
