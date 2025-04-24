6221ea53815e2481873b73d2f94d37fbab4b9285
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-04-25 00:14:34 +0200

    Add condition for flexible component

--- wp-content/themes/portfolio/template/template-about.php
+++ wp-content/themes/portfolio/template/template-about.php
@@ -1,7 +1,21 @@
 <?php /* Template Name: Page "À propos" */ ?>
 
-<?php get_header(); ?>
+<?php get_header();
 
-<?= get_the_content(); ?>
+include('partials/about/stage.php');
+
+if (have_rows('content')): while (have_rows('content')): the_row();
+    if (get_row_layout() === 'text-media-about'):
+        include('content/text-media/text-media-about.php');
+    elseif (get_row_layout() === 'school-step'):
+        include('content/school-step/school-step.php');
+    elseif (get_row_layout() === 'engagement'):
+        include('content/engagement/engagement.php');
+    elseif (get_row_layout() === 'language'):
+        include('content/language/language.php');
+    endif;
+endwhile;
+endif;
+?>
 
 <?php get_footer(); ?>
