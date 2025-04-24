fe8819d1805bbc1f1beb4a9b5060920fd277fec9
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-04-24 18:38:40 +0200

    Add language switch

--- wp-content/themes/portfolio/header.php
+++ wp-content/themes/portfolio/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="<?= __trans('fr'); ?>">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,9 +17,17 @@
         <ul class="nav__container">
             <?php foreach (dw_get_navigation_links('header') as $link): ?>
                 <li class="nav__item">
-                    <a href="<?= $link->href; ?>" class="nav__links" title="Aller sur la page : <?= $link->label?>">
+                    <a href="<?= $link->href; ?>" class="nav__links" title="Aller sur la page : <?= $link->label ?>">
                         <?= $link->label; ?>
                     </a>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+        <ul class="languages__container">
+            <?php foreach (pll_the_languages(['raw' => true, 'hide_current' => 1]) as $lang): ?>
+                <li class="languages__item<?= $lang['current_lang'] ? ' languages__item--current' : '' ?>">
+                    <a href="<?= $lang['url'] ?>" lang="<?= $lang['locale'] ?>" hreflang="<?= $lang['locale'] ?>"
+                       class="languages__link"><?= $lang['slug'] ?></a>
                 </li>
             <?php endforeach; ?>
         </ul>
