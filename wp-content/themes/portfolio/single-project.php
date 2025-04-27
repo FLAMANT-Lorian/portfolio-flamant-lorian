787f597f1c642338a3d3f154d452132369a616cf
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-04-27 11:37:19 +0200

    Add random projects section

--- wp-content/themes/portfolio/single-project.php
+++ wp-content/themes/portfolio/single-project.php
@@ -10,4 +10,32 @@ endwhile;
 endif;
 ?>
 
+    <section>
+        <h2>Découvrir d'autres projets</h2>
+        <div>
+            <?php
+            $projects = new WP_Query([
+                'post_type' => 'project',
+                'order' => 'DESC',
+                'orderby' => 'rand',
+                'posts_per_page' => 3,
+                'post__not_in' => [$post->ID],
+            ]);
+
+            if ($projects->have_posts()): while ($projects->have_posts()): $projects->the_post(); ?>
+
+                <article>
+                    <h3><?= get_the_title(); ?></h3>
+                    <figure>
+                        <img src="" alt="">
+                    </figure>
+                    <a href="<?= get_the_permalink(); ?>" class="" title="Découvrir le projet">
+                        <span class="sro">Découvrir le projet : <?= get_the_title(); ?></span>
+                    </a>
+                </article>
+
+            <?php endwhile; endif; ?>
+        </div>
+    </section>
+
 <?php get_footer(); ?>
