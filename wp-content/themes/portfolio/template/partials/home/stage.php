57f9ff828b733c1e7cdf7f74a363ac37a72e125b
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-05-02 10:20:23 +0200

    Starting CSS

--- wp-content/themes/portfolio/template/partials/home/stage.php
+++ wp-content/themes/portfolio/template/partials/home/stage.php
@@ -1,10 +1,12 @@
-<section>
-    <h2><?= get_field('main-title'); ?></h2>
+<section class="home__stage">
+    <h2 class="home--title">
+        <?= get_field('main-title'); ?>
+    </h2>
     <div>
         <h3><?= get_field('job-title'); ?></h3>
         <p><?= get_field('job-description'); ?></p>
     </div>
-    <?= responsive_image(get_field('profile-picture'), ['loading' => 'lazy', 'classes' => '']); ?>>
+    <?= responsive_image(get_field('profile-picture'), ['loading' => 'lazy', 'classes' => '']); ?>
     <a href="<?= get_field('high-link')['url']; ?>" title="Aller sur la page : À propos">
         <?= get_field('high-link')['title']; ?>
     </a>
