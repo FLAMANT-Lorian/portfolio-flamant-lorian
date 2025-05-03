05bce4b805b879cfedde346d5cf896c99ce2ea5d
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-05-03 15:34:14 +0200

    Update stage.php

--- wp-content/themes/portfolio/template/partials/project/stage.php
+++ wp-content/themes/portfolio/template/partials/project/stage.php
@@ -1,7 +1,12 @@
-<section>
+<section class="project">
     <?php if (have_rows('stage')): while (have_rows('stage')): the_row(); ?>
-        <h2><?= get_sub_field('main-title'); ?></h2>
-        <p><?= get_sub_field('projects-description', format_value: false); ?></p>
+        <h2 class="project--title">
+            <?= get_sub_field('main-title'); ?>
+            <span class="last__point">.</span>
+        </h2>
+        <p class="project--text">
+            <?= get_sub_field('projects-description', format_value: false); ?>
+        </p>
     <?php
     endwhile;
     endif;
