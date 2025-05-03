a4057ff791e07ea12d43f8e07ac1be0330b862ab
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-05-03 15:33:41 +0200

    Add burger menu function

---
+++ wp-content/themes/portfolio/resources/js/app/burger-menu.js
@@ -0,0 +1,9 @@
+const inputElt = document.querySelector('.bgm--checkbox');
+
+inputElt.addEventListener('change', function () {
+    if (inputElt.checked) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = 'initial';
+    }
+});
