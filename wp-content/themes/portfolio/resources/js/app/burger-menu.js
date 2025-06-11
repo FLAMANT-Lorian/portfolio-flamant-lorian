06cccfdc7acea8284195a8541509395d19f8a59c
FLAMANT-Lorian <lorian.flamant@student.hepl.be>
2025-06-11 20:37:47 +0200

    Updated burger menu

--- wp-content/themes/portfolio/resources/js/app/burger-menu.js
+++
@@ -1,9 +0,0 @@
-const inputElt = document.querySelector('.bgm--checkbox');
-
-inputElt.addEventListener('change', function () {
-    if (inputElt.checked) {
-        document.body.style.overflow = 'hidden';
-    } else {
-        document.body.style.overflow = 'initial';
-    }
-});
--- wp-content/themes/portfolio/resources/js/main.js
+++ wp-content/themes/portfolio/resources/js/main.js
@@ -126,12 +126,3 @@
     projectCard.init();
 })();
 document.documentElement.classList.add('js-enabled');
-const inputElt = document.querySelector('.bgm--checkbox');
-
-inputElt.addEventListener('change', function () {
-    if (inputElt.checked) {
-        document.body.style.overflow = 'hidden';
-    } else {
-        document.body.style.overflow = 'initial';
-    }
-});
